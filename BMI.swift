4a21d8a17624f99a91c05a81a8662d4721216e80
Justin Liao <justcs123only@gmail.com>
2025-06-22 20:36:29 -0700

    l0 - 2 basic program(temperature & BMI)

---
+++ BMI.swift
@@ -0,0 +1,6 @@
+// Description: Calculate and print the Body Mass Index (BMI)
+var weight: Double = 82
+var height: Double = 1.79
+var BMI: Double = weight/(height * height)
+
+print("My BMI is \(BMI).")
---
+++ temperature.swift
@@ -0,0 +1,6 @@
+// Description: This program converts a temperature from Fahrenheit to Celsius.
+var tempf: Double = 83.0
+var tempc: Double
+
+tempc = (tempf - 32)/1.8
+print("The temp is \(tempc) degrees Celsius.")
